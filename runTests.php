6c2624dd4350427a2df6afb2e45d88f61c591fcb
Bill Shupp <bshupp@empowercampaigns.com>
2010-09-27 11:42:55 -0700

    Updated runTests to run Deneb_AllTests by default if no arguments are present

--- runTests.php
+++ runTests.php
@@ -1,7 +1,7 @@
 <?php
 
 $base = dirname(__FILE__);
-set_include_path(get_include_path() . ":{$base}:{$base}/tests");
+set_include_path("{$base}:{$base}/tests:" .  get_include_path());
 
 require_once 'PHPUnit/Util/Filter.php';
 
@@ -10,6 +10,10 @@ PHPUnit_Util_Filter::addFileToWhitelist('Deneb.php');
 PHPUnit_Util_Filter::addDirectoryToWhitelist('Deneb');
 
 require_once 'PHPUnit/TextUI/Command.php';
+
+if (!isset($argv[1])) {
+    $argv[1] = 'tests/Deneb/AllTests.php';
+}
 
 $command = new PHPUnit_TextUI_Command;
 $command->run($argv);
